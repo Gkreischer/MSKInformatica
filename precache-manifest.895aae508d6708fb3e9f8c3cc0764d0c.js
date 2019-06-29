e25662155a293ff69a383447a6fbcde6f8575982
Gkreischer <gustavokreischer@gmail.com>
2019-06-29 20:05:46 -0300

    Updates

---
+++ precache-manifest.895aae508d6708fb3e9f8c3cc0764d0c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd785ec310c8509fa43f48333d169918",
+    "url": "/MSKInformatica/index.html"
+  },
+  {
+    "revision": "4dd70e69e988e706f0e8",
+    "url": "/MSKInformatica/static/css/2.54faf5fa.chunk.css"
+  },
+  {
+    "revision": "dd2164044a18edbeb65e",
+    "url": "/MSKInformatica/static/css/main.29bc844a.chunk.css"
+  },
+  {
+    "revision": "4dd70e69e988e706f0e8",
+    "url": "/MSKInformatica/static/js/2.856e8481.chunk.js"
+  },
+  {
+    "revision": "dd2164044a18edbeb65e",
+    "url": "/MSKInformatica/static/js/main.fd2146df.chunk.js"
+  },
+  {
+    "revision": "d134dc338bef5826d03b",
+    "url": "/MSKInformatica/static/js/runtime~main.ba50c129.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MSKInformatica/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MSKInformatica/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MSKInformatica/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MSKInformatica/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MSKInformatica/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
